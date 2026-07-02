e819d20e231413b1621150c41862d12da5b7985f
guihgalegodev <guigalego96@gmail.com>
2026-07-01 21:52:36 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import initTabNav from "./modules/tabnav.js";
 import initAccordionList from "./modules/accordion.js";
+import activeModal from "./modules/modal.js";
 
 initAnimacaoScroll();
 initScrollSuave();
 initTabNav();
 initAccordionList();
+activeModal();
