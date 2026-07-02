0b575e15c2d895eab2a6069b04418de4e0138311
guihgalegodev <guigalego96@gmail.com>
2026-07-02 12:29:09 -0300

    tooltip box

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initTabNav from "./modules/tabnav.js";
 import initAccordionList from "./modules/accordion.js";
 import activeModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initAnimacaoScroll();
 initScrollSuave();
 initTabNav();
 initAccordionList();
 activeModal();
+initTooltip();
